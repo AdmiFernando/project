2d3e664689e049f675e9e0a1c49b81b30fe139f3
Ezfran <ezfranquijada.com>
2024-02-02 00:50:42 -0400

    fix render spring/nextjs

--- my-app/dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"yaxAbjr+Wny++VO4BLjEBNO9hYhtYfennh33kx8QBdE=\"}"
--- my-app/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    //    Optional: Change the output directory `out` -> `dist`
-    distDir: 'out',
-};
-
-export default nextConfig;
