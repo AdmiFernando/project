9409d0d34a912a9550b246c9ac83166c94126e81
Alejandro Zerpa <105513547+azerpa56@users.noreply.github.com>
2024-04-26 21:20:43 -0400

    Add files via upload

--- nextjs-front/src/app/components/header.js
+++ nextjs-front/src/app/components/header.js
@@ -22,7 +22,7 @@ const Header = () => {
         </div>
         <ul className="hidden gap-10 items-center uppercase text-black lg:text-white md:flex font-semibold tracking-wider">
           <NavLink href='#about'>About Us</NavLink>
-          <NavLink href='#expertise'>Expertise</NavLink>
+          {/* <NavLink href='#expertise'>Expertise</NavLink> */}
           <NavLink href='#contact'>Contact</NavLink>
         </ul>
 
@@ -35,26 +35,26 @@ const Header = () => {
         </nav>
 
         <div onClick={() => setIsActive(!isActive)} className={isActive ? "fixed h-[100vh] w-full bg-[rgba(0,0,0,.8)] left-0 bottom-0 duration-500 transition-all translate-y-[0vh]" : "duration-700 w-full h-[100vh] left-0 bottom-0 transition-all absolute -translate-y-[100vh]"}>
-        <nav className="z-50 absolute right-0 pt-[53px] md:pt-[70px] pr-3 md:pr-8">
-          <button onClick={() => setIsActive(!isActive)} className="flex items-center flex-col gap-2 ">
-            <div className="h-0.5 w-10 bg-white"></div>
-            <div className="h-0.5 w-10 bg-white"></div>
-            <div className="h-0.5 w-10 bg-white"></div>
-          </button>
-        </nav>
-          <ul className="flex justify-center items-center flex-col h-full text-white gap-8 text-lg sm:text-xl md:text-2xl">
-            <ul className="flex flex-wrap justify-center md:hidden gap-4 md:gap-10 items-center uppercase text-sm text-white lg:text-white font-semibold tracking-wider">
-              <NavLink className="border-b-2 pb-0.5" href='#about'>About Us</NavLink>
-              <NavLink className="border-b-2 pb-0.5" href='#expertise'>Expertise</NavLink>
-              <NavLink className="border-b-2 pb-0.5" href='#contact'>Contact</NavLink>
+          <nav className="z-50 absolute right-0 pt-[53px] md:pt-[70px] pr-3 md:pr-8">
+            <button onClick={() => setIsActive(!isActive)} className="flex items-center flex-col gap-2 ">
+              <div className="h-0.5 w-10 bg-white"></div>
+              <div className="h-0.5 w-10 bg-white"></div>
+              <div className="h-0.5 w-10 bg-white"></div>
+            </button>
+          </nav>
+            <ul className="flex justify-center items-center flex-col h-full text-white gap-8 text-lg sm:text-xl md:text-2xl">
+              <ul className="flex flex-wrap justify-center md:hidden gap-4 md:gap-10 items-center uppercase text-sm text-white lg:text-white font-semibold tracking-wider">
+                <NavLink className="border-b-2 pb-0.5" href='#about'>About Us</NavLink>
+                {/* <NavLink className="border-b-2 pb-0.5" href='#expertise'>Expertise</NavLink> */}
+                <NavLink className="border-b-2 pb-0.5" href='#contact'>Contact</NavLink>
+              </ul>
+              <NavLink href="#frame">Languages/Frameworks</NavLink>
+              <NavLink href="#">Database Management</NavLink>
+              <NavLink href="#">Version Control</NavLink>
+              <NavLink href="#">Expertise</NavLink>
+              <NavLink href="#">Project Management</NavLink>
+              <NavLink href="#">Excluded Services</NavLink>
             </ul>
-            <NavLink href="#">Languages/Frameworks</NavLink>
-            <NavLink href="#">Database Management</NavLink>
-            <NavLink href="#">Version Control</NavLink>
-            <NavLink href="#">Expertise</NavLink>
-            <NavLink href="#">Project Management</NavLink>
-            <NavLink href="#">Excluded Services</NavLink>
-          </ul>
         </div>
 
       </header>
---
+++ nextjs-front/src/app/components/modalexclude.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import Image from 'next/image';
+
+export default function ExcludeWindows() {
+
+  const [isopen, setIsopen] = useState(false)
+  return (
+    <>
+    <div className='flex flex-row justify-center items-center text-white'>
+
+      <h5 className="w-[60%] text-center m-0 text-3xl tracking-widest font-thin hover:text-[#1F803F]">
+       Exclude Services
+      </h5>
+
+      <button className="w-[12%] max-sm:w-[15%] max-sm:m-2 hover:scale-125  m- p-0 font-bold" onClick={()=>setIsopen(true)}>
+      <svg className='fill-white w-full hover:fill-[#1F803F]' xmlns="http://www.w3.org/2000/svg" id="arrow-circle-down" viewBox="0 0 24 24">
+          <path d="M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,16a2.993,2.993,0,0,1-1.987-.752c-.327-.291-.637-.574-.84-.777L6.3,11.647a1,1,0,0,1,1.4-1.426L10.58,13.05c.188.187.468.441.759.7a1,1,0,0,0,1.323,0c.29-.258.57-.512.752-.693L16.3,10.221a1,1,0,1,1,1.4,1.426l-2.879,2.829c-.2.2-.507.48-.833.769A2.99,2.99,0,0,1,12,16Z"/><path xmlns="http://www.w3.org/2000/svg" d="M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,16a2.993,2.993,0,0,1-1.987-.752c-.327-.291-.637-.574-.84-.777L6.3,11.647a1,1,0,0,1,1.4-1.426L10.58,13.05c.188.187.468.441.759.7a1,1,0,0,0,1.323,0c.29-.258.57-.512.752-.693L16.3,10.221a1,1,0,1,1,1.4,1.426l-2.879,2.829c-.2.2-.507.48-.833.769A2.99,2.99,0,0,1,12,16Z"/>
+      </svg>
+      </button>
+    </div>
+      {
+        isopen && (
+          <div className="inset-0 absolute backdrop-blur-sm flex justify-center items-center max-sm:w-[100%] max-sm:h-[100%]">
+
+            <div className="bg-[#062132] text-white flex flex-col items-center p-5 rounded w-full h-[100%] max-sm:w-[100%]">
+
+            <header className="text-center p-5">
+              <h1 className="text-5xl">About Us</h1>
+            </header>
+
+            <div className='flex flex-row max-sm:flex-col'>
+              <div className='flex flex-col'>
+
+              <div className='flex flex-row items-center gap-0 text-white'>
+
+                  <h5 className="w-[50%] max-sm:w-[80%] text-center m-0 text-3xl tracking-widest font-thin hover:text-[#1F803F]">
+                    Version Control
+                  </h5>
+  
+                  <button className="w-[4%] max-sm:w-[13%] hover:scale-125  m- p-0 font-bold" onClick={()=>setIsopen(false)}>
+                        
+                        <svg className='fill-white w-full hover:fill-[#1F803F]' xmlns="http://www.w3.org/2000/svg" id="arrow-circle-down" viewBox="0 0 24 24"><path d="M12,24A12,12,0,1,0,0,12,12.013,12.013,0,0,0,12,24ZM12,8a2.993,2.993,0,0,1,1.987.752c.327.291.637.574.84.777L17.7,12.353a1,1,0,1,1-1.4,1.426L13.42,10.95c-.188-.187-.468-.441-.759-.7a1,1,0,0,0-1.323,0c-.29.258-.57.512-.752.693L7.7,13.779a1,1,0,0,1-1.4-1.426L9.178,9.524c.2-.2.507-.48.833-.769A2.99,2.99,0,0,1,12,8Z"/><path xmlns="http://www.w3.org/2000/svg" d="M12,24A12,12,0,1,0,0,12,12.013,12.013,0,0,0,12,24ZM12,8a2.993,2.993,0,0,1,1.987.752c.327.291.637.574.84.777L17.7,12.353a1,1,0,1,1-1.4,1.426L13.42,10.95c-.188-.187-.468-.441-.759-.7a1,1,0,0,0-1.323,0c-.29.258-.57.512-.752.693L7.7,13.779a1,1,0,0,1-1.4-1.426L9.178,9.524c.2-.2.507-.48.833-.769A2.99,2.99,0,0,1,12,8Z"/>
+                        </svg>
+                  </button>
+                </div>
+
+                <div className='m-5 flex flex-col justify-center'>
+                  <div className='m-2'>
+
+                    <p className='text-2xl text-justify max-sm:m-0 m-5'>
+                    Please note that we do not provide network and server infrastructure Services<i className='text-4xl'>.</i>
+                    </p>
+                  </div>
+                  </div>
+              </div>
+              <div>
+                  <Image
+                    src="/excluded.png"
+                    alt="Excluded Logo"
+                    width={1920}
+                    height={1080}
+                    className="w-full h-full object-cover"
+                  />
+              </div>
+
+            </div>
+              <div>
+              
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </>
+  );
+}
