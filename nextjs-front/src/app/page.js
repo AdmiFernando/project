5a8d56dc4f05386dfed1cf595d13573cef93f71a
Alejandro Zerpa <105513547+azerpa56@users.noreply.github.com>
2024-05-16 00:00:09 -0400

    Expertise section
    Transitions between components and Expertise section

--- nextjs-front/src/app/page.js
+++ nextjs-front/src/app/page.js
@@ -1,25 +1,26 @@
 'use client'
 import React from "react";
-import "./page.css";
-import "./components/transitionStyles.css";
-import Main from "./components/main";
+import "./components/page.css";
+import "./components/transitionStyles.css"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from "./components/layout";
 import Home from "./components/home";
 import About from "./components/about";
 import Service from "./components/service";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Expertise from "./components/expertise";
 
 export default function App() {
-
   return (
     <Router>
-          <Routes>
-            <Route path="/" element={<Main />}>
-              <Route index element={<Home />} />
-              <Route path="home" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="service" element={<Service />} />
-            </Route>
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="service" element={<Service />} />
+          <Route path="expertise" element={<Expertise />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
